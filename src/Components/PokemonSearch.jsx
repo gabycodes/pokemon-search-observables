b863c052f85773c776aba8cc5a5a86c902ae5f15
Gaby Küpfer <gabykupfer@Gabys-MacBook-Pro.local>
2019-12-11 12:37:14 -0500

    Hooks refactor

--- src/Components/PokemonSearch.jsx
+++ src/Components/PokemonSearch.jsx
@@ -1,41 +1,32 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import SearchForm from './SearchForm'
 import ResultsList from './ResultsList'
 
-class PokeonSearch extends Component {
-	// create form with throttle
-	// every nth second, results populate with applicable pokemon
-	// select pokemon to open more details
+const PokeonSearch = () => {
+	const [currentQuery, setCurrentQuery] = useState('')
+	const [searchResults, setSearchResults] = useState([])
 
-
-	state = {
-		currentQuery: '',
-		searchResults: []
-	}
-
-	updateCurrentQuery = newQuery => {
-		this.setState({ currentQuery: newQuery})
-		this.searchPokemon(newQuery)
+	const updateCurrentQuery = newQuery => {
+		setCurrentQuery(newQuery)
+		searchPokemon(newQuery)
 	}
 	
-	searchPokemon = query => {
+	const searchPokemon = query => {
 		fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=1000')
 			.then(res => res.json())
 			.then(res => res.results.filter(pokemon => pokemon.name.includes(query)))
 			.then(res => res.filter(pokemon => !pokemon.name.includes('alola')))
-			.then(res => this.setState({ searchResults: res }))
+			.then(res => setSearchResults(res))
 	}
 
-	render() {
 		return(
 			<>
 				<SearchForm 
-					updateCurrentQuery={this.updateCurrentQuery} 
+					updateCurrentQuery={updateCurrentQuery} 
 				/>
-				<ResultsList results={this.state.searchResults} />
+				<ResultsList results={searchResults} />
 			</>
 		)
-	}
 }
 
 export default PokeonSearch
